71949ebebca8bca6d77e94d009e879bc16f5a2bc
royyy1999 <royyy1999@CIS-RDS2>
2020-09-05 14:17:08 -0500

    2:18 PM

--- DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
+++ DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
@@ -1,10 +1,11 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Edited by: Roy Fernandez
  * Class: DragonbornWaffleFriesTests.cs
  * Purpose: Test the DragonbornWaffleFries.cs class in the Data library
  */
 using Xunit;
-
+using BleakwindBuffet.Data.Sides;
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
 
@@ -15,16 +16,27 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            Assert.Equal(Size.Small, df.Size);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            df.Size = Size.Large;
+            Assert.Equal(Size.Large, df.Size);
+            df.Size = Size.Medium;
+            Assert.Equal(Size.Medium, df.Size);
+            df.Size = Size.Small;
+            Assert.Equal(Size.Small, df.Size);
         }
-
+        
         [Fact]
         public void ShouldReturnCorrectSpecialInstructions()
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            Assert.Empty(df.SpecialInstructions);
         }
 
         [Theory]
@@ -33,6 +45,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 0.96)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            df.Size = size;
+            Assert.Equal(price, df.Price);
         }
 
         [Theory]
@@ -41,6 +56,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 100)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            df.Size = size;
+            Assert.Equal(calories, df.Calories);
         }
 
         [Theory]
@@ -49,6 +67,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, "Large Dragonborn Waffle Fries")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            DragonbornWaffleFries df = new DragonbornWaffleFries();
+            df.Size = size;
+            Assert.Equal(name, df.ToString());
         }
     }
 }
--- DataTests/UnitTests/SideTests/MadOtarGritsTests.cs
+++ DataTests/UnitTests/SideTests/MadOtarGritsTests.cs
@@ -1,10 +1,11 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Edited by: Roy Fernandez
  * Class: MadOtarGritsTests.cs
  * Purpose: Test the MadOtarGrits.cs class in the Data library
  */
 using Xunit;
-
+using BleakwindBuffet.Data.Sides;
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
 
@@ -15,16 +16,27 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            Assert.Equal(Size.Small, mg.Size);
         }
                 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            mg.Size = Size.Large;
+            Assert.Equal(Size.Large, mg.Size);
+            mg.Size = Size.Medium;
+            Assert.Equal(Size.Medium, mg.Size);
+            mg.Size = Size.Small;
+            Assert.Equal(Size.Small, mg.Size);
         }
 
         [Fact]
         public void ShouldReturnCorrectStringOnSpecialInstructions()
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            Assert.Empty(mg.SpecialInstructions);
         }
 
         [Theory]
@@ -33,6 +45,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 1.93)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            mg.Size = size;
+            Assert.Equal(price, mg.Price);
         }
 
         [Theory]
@@ -41,6 +56,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 179)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            mg.Size = size;
+            Assert.Equal(calories, mg.Calories);
         }
 
         [Theory]
@@ -49,6 +67,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, "Large Mad Otar Grits")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            MadOtarGrits mg = new MadOtarGrits();
+            mg.Size = size;
+            Assert.Equal(name, mg.ToString());
         }
     }
 }
