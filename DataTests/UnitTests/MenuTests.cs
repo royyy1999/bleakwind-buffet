a885378595d298c495e243686c5fd76701d51a51
royyy1999 <royyy1999@CIS-RDS2>
2020-09-11 11:38:46 -0500

    11:39 AM

--- DataTests/UnitTests/MenuTests.cs
+++ DataTests/UnitTests/MenuTests.cs
@@ -4,7 +4,7 @@
  * Purpose: Test the Menu.cs class in the Data library
  */
 using Xunit;
-
+using System.Collections.Generic;
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Entrees;
 using BleakwindBuffet.Data.Drinks;
@@ -17,26 +17,159 @@ namespace BleakwindBuffet.DataTests.UnitTests
         [Fact]
         public void ShouldHaveCorrectListOfEntrees()
         {
-            Assert.Collection<Entree>(Menu.Entrees());
-            
+            bool includesBB = false;
+            bool includesDD = false;
+            bool includesGO = false;
+            bool includesPP = false;
+            bool includesSS = false;
+            bool includesTT = false;
+            bool includesTB = false;
+
+            int expLength = 7;
+
+            IEnumerable<IOrderItem> entree = Menu.Entrees();
+            List<IOrderItem> list = (List<IOrderItem>)entree;
+
+            foreach (IOrderItem item in list)
+            {
+                if (item is BriarheartBurger)
+                {
+                    includesBB = true;
+                }
+                else if (item is DoubleDraugr) {
+                    includesDD = true;
+                }
+                else if (item is GardenOrcOmelette)
+                {
+                    includesGO = true;
+                }
+                else if (item is PhillyPoacher)
+                {
+                    includesPP = true;
+                }
+                else if (item is SmokehouseSkeleton)
+                {
+                    includesSS = true;
+                }
+                else if (item is ThalmorTriple)
+                {
+                    includesTT = true;
+                }
+                else if (item is ThugsTBone)
+                {
+                    includesTB = true;
+                }
+            }
+
+            Assert.True(includesBB);
+            Assert.True(includesDD);
+            Assert.True(includesGO);
+            Assert.True(includesPP);
+            Assert.True(includesSS);
+            Assert.True(includesTT);
+            Assert.True(includesTB);
+            Assert.Equal(expLength, list.Count);
+
         }
+
 
         [Fact]
         public void ShouldHaveCorrectListOfSides()
         {
-            Assert.Collection<Side>();
+            bool includesDW = false;
+            bool includesFM = false;
+            bool includesMO = false;
+            bool includesVS = false;
+
+            int expLength = 12;
+
+            IEnumerable<IOrderItem> side = Menu.Sides();
+            List<IOrderItem> list = (List<IOrderItem>)side;
+
+            foreach (IOrderItem item in list)
+            {
+                if (item is DragonbornWaffleFries)
+                {
+                    includesDW = true;
+                }
+                else if (item is FriedMiraak)
+                {
+                    includesFM = true;
+                }
+                else if (item is MadOtarGrits)
+                {
+                    includesMO = true;
+                }
+                else if (item is VokunSalad)
+                {
+                    includesVS = true;
+                }
+            }
+
+            Assert.True(includesDW);
+            Assert.True(includesFM);
+            Assert.True(includesMO);
+            Assert.True(includesVS);
+            Assert.Equal(expLength, list.Count);
         }
 
         [Fact]
         public void ShouldHaveCorrectListOfDrinks()
         {
-            Assert.Collection<Drink>();
-        }
 
+            bool includesAJ = false;
+            bool includesCC = false;
+            bool includesMM = false;
+            bool includesSS = false;
+            bool includesWW = false;
+
+            int expLength = 30;
+
+            IEnumerable<IOrderItem> drink = Menu.Drinks();
+            List<IOrderItem> list = (List<IOrderItem>)drink;
+
+            foreach (IOrderItem item in list)
+            {
+                if (item is AretinoAppleJuice)
+                {
+                    includesAJ = true;
+                }
+                else if (item is CandlehearthCoffee)
+                {
+                    includesCC = true;
+                }
+                else if (item is MarkarthMilk)
+                {
+                    includesMM = true;
+                }
+                else if (item is SailorSoda)
+                {
+                    includesSS = true;
+                }
+                else if (item is WarriorWater)
+                {
+                    includesWW = true;
+                }          
+            }
+
+            Assert.True(includesAJ);
+            Assert.True(includesCC);
+            Assert.True(includesMM);
+            Assert.True(includesSS);
+            Assert.True(includesWW);
+            Assert.Equal(expLength, list.Count);
+        }
+    
+
+        
         [Fact]
         public void ShouldHaveCorrectListOfFullMenu()
         {
-            Assert.Collection<IOrderItem>(Menu.FullMenu());
+
+            int numberOfItemsInSizeEnum = Enum.GetValues(typeof(Size)).Length;
+
         }
+        
     }
 }
+        
